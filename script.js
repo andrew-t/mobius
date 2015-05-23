0ae603ddd07bcb14480758f4a41fec3344bea3f0
Andrew Taylor <mac@andrewt.net>
2015-05-23 11:42:39 +0100

    Use container layer
    Should fix mobile and aid embedding

--- script.js
+++ script.js
@@ -1,94 +1,118 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var current = i(),
-		topology = {
-			square: {
-				horizontal: null,
-				vertical: null
-			},
-			'h-cylinder': {
-				horizontal: i(),
-				vertical: null
-			},
-			'v-cylinder': {
-				horizontal: null,
-				vertical: i()
-			},
-			'h-mobius': {
-				horizontal: vFlip(),
-				vertical: null
-			},
-			'v-mobius': {
-				horizontal: null,
-				vertical: hFlip()
-			},
-			torus: {
-				horizontal: i(),
-				vertical: i()
-			},
-			'h-klein': {
-				horizontal: vFlip(),
-				vertical: i()
-			},
-			'v-klein': {
-				horizontal: i(),
-				vertical: hFlip()
-			},
-			rp2: {
-				horizontal: vFlip(),
-				vertical: hFlip()
-			}
-		}[document.body.getAttribute('data-topology')],
-		layers = content();
+	var els = document.getElementsByClassName('topological');
+	for (var eli = 0; eli < els.length; ++eli) (function(el) {
+		var current = i(),
+			topology = {
+				square: {
+					horizontal: null,
+					vertical: null
+				},
+				'h-cylinder': {
+					horizontal: i(),
+					vertical: null
+				},
+				'v-cylinder': {
+					horizontal: null,
+					vertical: i()
+				},
+				'h-mobius': {
+					horizontal: vFlip(),
+					vertical: null
+				},
+				'v-mobius': {
+					horizontal: null,
+					vertical: hFlip()
+				},
+				torus: {
+					horizontal: i(),
+					vertical: i()
+				},
+				'h-klein': {
+					horizontal: vFlip(),
+					vertical: i()
+				},
+				'v-klein': {
+					horizontal: i(),
+					vertical: hFlip()
+				},
+				rp2: {
+					horizontal: vFlip(),
+					vertical: hFlip()
+				}
+			}[el.getAttribute('data-topology')],
+			layers = content();
 
-	reset();
-	window.addEventListener('resize', reset);
-	window.addEventListener('scroll', update);
+		reset();
+		window.addEventListener('resize', reset);
+		el.addEventListener('scroll', update);
 
-	function reset() {
-		if (topology.horizontal)
-			document.body.scrollLeft = document.body.clientWidth;
-		if (topology.vertical)
-			document.body.scrollTop = document.body.clientHeight;
-		update();
-	}
-
-	function update() {
-		if (topology.horizontal) {
-			if (document.body.scrollLeft < document.body.clientWidth) {
-				document.body.scrollLeft += document.body.clientWidth;
-				current = current.x(topology.horizontal);
-			}
-			if (document.body.scrollLeft >= 2 * document.body.clientWidth) {
-				document.body.scrollLeft -= document.body.clientWidth;
-				current = current.x(topology.horizontal);
-			}
+		function reset() {
+			if (topology.horizontal)
+				el.scrollLeft = el.clientWidth;
+			if (topology.vertical)
+				el.scrollTop = el.clientHeight;
+			update();
 		}
-		if (topology.vertical) {
-			if (document.body.scrollTop < document.body.clientHeight) {
-				document.body.scrollTop += document.body.clientHeight;
-				current = current.x(topology.vertical);
-			}
-			if (document.body.scrollTop >= 2 * document.body.clientHeight) {
-				document.body.scrollTop -= document.body.clientHeight;
-				current = current.x(topology.vertical);
-			}
-		}
-		t(0, 0);
-		t(1, 0, topology.horizontal);
-		t(0, 1, topology.vertical);
-		t(1, 1, topology.horizontal, topology.vertical);
-	}
 
-	function content() {
-		var buffer = document.getElementById('buffer'),
-			layers = document.getElementsByClassName('main');
-		if (topology.horizontal)
-			buffer.style.left = '300vw';
-		if (topology.vertical)
-			buffer.style.top = '300vh';
-		return [ [ layers[0], layers[1] ],
-		         [ layers[2], layers[3] ] ];
-	}
+		function update() {
+			if (topology.horizontal) {
+				if (el.scrollLeft < el.clientWidth) {
+					el.scrollLeft += el.clientWidth;
+					current = current.x(topology.horizontal);
+				}
+				if (el.scrollLeft >= 2 * el.clientWidth) {
+					el.scrollLeft -= el.clientWidth;
+					current = current.x(topology.horizontal);
+				}
+			}
+			if (topology.vertical) {
+				if (el.scrollTop < el.clientHeight) {
+					el.scrollTop += el.clientHeight;
+					current = current.x(topology.vertical);
+				}
+				if (el.scrollTop >= 2 * el.clientHeight) {
+					el.scrollTop -= el.clientHeight;
+					current = current.x(topology.vertical);
+				}
+			}
+			t(0, 0);
+			t(1, 0, topology.horizontal);
+			t(0, 1, topology.vertical);
+			t(1, 1, topology.horizontal, topology.vertical);
+		}
+
+		function content() {
+			var buffer = document.getElementById('buffer'),
+				layers = document.getElementsByClassName('main');
+			if (topology.horizontal)
+				buffer.style.left = '300vw';
+			if (topology.vertical)
+				buffer.style.top = '300vh';
+			return [ [ layers[0], layers[1] ],
+			         [ layers[2], layers[3] ] ];
+		}
+
+		function t(x, y) {
+			var matrix = current;
+			for (var i = 2; i < arguments.length; ++i)
+				if (arguments[i])
+					matrix = matrix.x(arguments[i]);
+				else {
+					if (layers[x][y]) {
+						el.removeChild(layers[x][y]);
+						layers[x][y] = null;
+					}
+					return;
+				}
+			layers[x][y].style.transform = 
+				'translate(' + 
+					(topology.horizontal ? x * 100 + 100 : 0) + 'vw, ' +
+					(topology.vertical ? y * 100 + 100 : 0) + 'vh) ' +
+				'matrix(' + matrix.elements.map(function(row) {
+					return row[0] + ',' + row[1];
+				}).join() + ')';
+		}
+	})(els[eli]);
 
 	function i() {
 		return Sylvester.Matrix.I(3);
@@ -102,26 +126,5 @@ document.addEventListener('DOMContentLoaded', function() {
 		return $M([ [  1,  0,  0 ],
 		            [  0, -1,  0 ],
 		            [  0,  0,  0 ] ]);
-	}
-
-	function t(x, y) {
-		var matrix = current;
-		for (var i = 2; i < arguments.length; ++i)
-			if (arguments[i])
-				matrix = matrix.x(arguments[i]);
-			else {
-				if (layers[x][y]) {
-					document.body.removeChild(layers[x][y]);
-					layers[x][y] = null;
-				}
-				return;
-			}
-		layers[x][y].style.transform = 
-			'translate(' + 
-				(topology.horizontal ? x * 100 + 100 : 0) + 'vw, ' +
-				(topology.vertical ? y * 100 + 100 : 0) + 'vh) ' +
-			'matrix(' + matrix.elements.map(function(row) {
-				return row[0] + ',' + row[1];
-			}).join() + ')';
 	}
 });
